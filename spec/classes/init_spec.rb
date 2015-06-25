8b24358a0f7fedbcb87086aafd09136d525137fb
scott coulton <scott.coulton@healthdirect.org.au>
2015-06-25 21:00:27 +1000

    Fixed spec test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 describe 'swarm' do
 
+let(:facts) { {:osfamily => 'RedHat', :operatingsystemrelease => 'RedHat Linux release 6.5'}}
+  
   context 'with defaults for all parameters' do
     it { should contain_class('swarm') }
   end
